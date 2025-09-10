fb9f35f17427cc8289b868f7420c551fd9549c2e
sibi_tiruchi <sibi.tiruchi@gmail.com>
2025-09-10 11:48:25 -0400

    dockerized

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const API_BASE = "http://127.0.0.1:8000/api";
+// const API_BASE = "http://0.0.0.0:8000/api";
 
 export const getHypothesis1 = async () => {
-  const { data } = await axios.get(`${API_BASE}/hypothesis-1`); "http://127.0.0.1:8000/api/hypothesis-1"
+  const { data } = await axios.get(`${API_BASE}/hypothesis-1`);
   return data;
 };
 
